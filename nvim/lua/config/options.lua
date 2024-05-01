4ab5cb376a976fde2a30ab66c06ccd26529b9482
jeyren95 <jeyren95@hotmail.com>
2024-05-02 00:56:42 +0800

    Added initial nvim config files

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,10 @@
+vim.opt.tabstop = 2 -- number of spaces used for each tab
+vim.opt.cmdheight = 2 -- command line height
+vim.opt.number = true -- whether to show line number 
+vim.opt.ignorecase = true -- ignore case in search patterns
+vim.opt.relativenumber = false -- number of lines relative from the cursor
+vim.opt.shiftwidth = 2 -- number of spaaces used for each indent
+vim.opt.smartindent = true -- smart indenting when starting a new line
+vim.opt.smartcase = true -- override the ignorecase option if search pattern contains uppercase
+vim.opt.splitright = true -- splits the new window to the right
+vim.opt.mouse = "a" -- allow mouse to: be used in neovim
---
+++ nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,15 @@
+return {
+	  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+	  require("tokyonight").setup({
+		  style = "night",
+		  styles = {
+			  comments = { italic = false },
+			  keywords = { italic = false },
+		  }
+	  })
+  end,
+  }
