0a9954c5516fd229eec9de47e8226876a4176ff7
jeyren95 <jeyren95@hotmail.com>
2024-05-09 22:25:58 +0800

    Updated nvim tree keymapping

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -16,16 +16,26 @@ return {
 		local on_attach = function(client, bufnr)
 			opts.bufnr = bufnr
 			-- default 
-			api.config.mappings.default_on_attach(bufnr)
+			-- api.config.mappings.default_on_attach(bufnr)
 
 			-- custom
 			keymap.set("n", "<leader>tt", api.tree.toggle, opts)
 			keymap.set("n", "<leader>s", api.node.open.vertical, opts)
 			keymap.set("n", "<leader>n", api.fs.create, opts)
 			keymap.set("n", "<leader>d", api.fs.remove, opts)
-			keymap.set("n", "<leader>c", api.fs.copy.node, opts)
+			keymap.set("n", "<leader>y", api.fs.copy.node, opts)
 			keymap.set("n", "<leader>p", api.fs.paste, opts)
 			keymap.set("n", "<leader>r", api.fs.rename, opts)
+			keymap.set("n", "<BS>", api.node.navigate.parent_close, opts)
+			keymap.set("n", "<CR>", api.node.open.edit, opts)
+			keymap.set("n", "E", api.tree.expand_all, opts)
+			keymap.set("n", "]e", api.node.navigate.diagnostics.next, opts)
+			keymap.set("n", "[e", api.node.navigate.diagnostics.prev, opts)
+			keymap.set("n", "<leader>f", api.live_filter.start, opts)
+			keymap.set("n", "<leader>o", api.node.open.edit, opts)
+			keymap.set("n", "<leader>x", api.fs.cut, opts)
+			keymap.set("n", "<leader>yn", api.fs.copy.filename, opts)
+
 		end
 
 		nvim_tree.setup({
