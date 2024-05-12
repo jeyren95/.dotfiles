fa4071391092d494e4b749810da37bf16671d9c4
jeyren95 <jeyren95@hotmail.com>
2024-05-12 14:56:02 +0800

    Added toggle term

---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+	"akinsho/toggleterm.nvim",
+	config = function ()
+		local toggleterm = require("toggleterm")
+		toggleterm.setup({
+			size = 28,
+			open_mapping = [[<c-\>]],
+			direction = "horizontal",
+		})
+	end
+}
