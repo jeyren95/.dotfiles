9f2dcb9b580e84a117ba398926ffa5e33db62ab8
jeyren95 <jeyren95@hotmail.com>
2025-02-17 14:52:12 +0800

    Updated custom config for onedark theme

--- nvim/lua/plugins/theme/onedark.lua
+++ nvim/lua/plugins/theme/onedark.lua
@@ -2,8 +2,14 @@ return {
 	"navarasu/onedark.nvim",
 	config = function()
 		require("onedark").setup({
-			style = "darker",
+			style = "deep",
 			toggle_style_key = "<leader>ts",
+			highlights = {
+				["@tag"] = { fg = "$red" },
+				["@tag.tsx"] = { fg = "$yellow" },
+				["@tag.attribute"] = { fg = "$orange" },
+				["@tag.delimiter"] = { fg = "none" }
+			}
 		})
 	end,
 }
